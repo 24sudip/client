e8baf4394142da9e876b4d24598ac30ab4610233
unknown <sudip2tenber@gmail.com>
2024-01-16 12:26:50 +0600

    client

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+
+$(function(){
+
+    $('.what_right_main').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay:true,
+        speed:1000,
+        arrows:true,
+        prevArrow:".prev1",
+        nextArrow:".next1"
+    });
+    
+})
